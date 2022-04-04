e30748b219a05c347f57df2bc8693d4010742d24
Kyle <40324912+mkyle625@users.noreply.github.com>
2022-04-04 11:37:13 -0400

    Project 4 setup, run npm install

---
+++ project-4/src/myconfig.ts
@@ -0,0 +1,32 @@
+// Import the functions you need from the SDKs you need
+
+import { initializeApp } from "firebase/app";
+
+// TODO: Add SDKs for Firebase products that you want to use
+
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+
+// Your web app's Firebase configuration
+
+const firebaseConfig = {
+
+  apiKey: "AIzaSyBc1DzyJkoxf7RiyvGrB7Dbseu1OpsEPTw",
+
+  authDomain: "project-4-643f1.firebaseapp.com",
+
+  projectId: "project-4-643f1",
+
+  storageBucket: "project-4-643f1.appspot.com",
+
+  messagingSenderId: "363939978137",
+
+  appId: "1:363939978137:web:0e8797f6999c824ebad108"
+
+};
+
+export {firebaseConfig};
+
+// Initialize Firebase
+
+const app = initializeApp(firebaseConfig);
---
+++ project-4/src/views/HomeView.vue
@@ -0,0 +1,45 @@
+<template>
+  <div class="home">
+    <h2>This is the main page. I got here via {{byWayOf}}</h2>
+    <p>{{userInfo}}</p>
+    <img :src="userPhotoURL" v-if="userPhotoURL.length > 0" width="64">
+    <button @click="outtahere">Logout</button>
+  </div>
+</template>
+
+<script lang="ts">
+import { Component, Prop, Vue } from "vue-property-decorator";
+import WorldTime from "../components/70-world-time.vue";
+import {
+  getAuth,
+  onAuthStateChanged,
+  User,
+  Auth,
+  signOut,
+} from "firebase/auth"; 
+@Component
+export default class HomeView extends Vue {
+  @Prop() readonly byWayOf!: string;
+
+  userPhotoURL = "";
+  auth: Auth | null = null;
+  userInfo = "";
+  mounted(): void {
+    this.auth = getAuth();
+    onAuthStateChanged(this.auth, (user: User | null) => {
+      console.log("Auth changed", user);
+      if (user) {
+        this.userPhotoURL = user.photoURL ?? "";
+        this.userInfo = `${user.displayName ?? "No Name"}`;
+      }
+    });
+  }
+
+  outtahere(): void {
+    if (this.auth) signOut(this.auth);
+
+    // Back to the previous page
+    this.$router.back();
+  }
+}
+</script>
